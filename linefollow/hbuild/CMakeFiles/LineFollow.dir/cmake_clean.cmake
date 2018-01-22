03b7ee7d27700450e6abffbc9c67cedc5ba46b5b
mm6567 <6567mm@gmail.com>
2018-01-21 16:49:59 -0800

    first

---
+++ linefollow/hbuild/CMakeFiles/LineFollow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LineFollow.dir/src/Modules/LineFollow/LineFollow.C.o"
+  "LineFollow.pdb"
+  "LineFollow.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LineFollow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ linefollow/hbuild/CMakeFiles/modinfo_LineFollow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modinfo_LineFollow"
+  "../src/Modules/LineFollow/modinfo.yaml"
+  "../src/Modules/LineFollow/modinfo.html"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/modinfo_LineFollow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
