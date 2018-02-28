6a2ff5040f28ac4c971290615b70938ddbbcf55d
mm6567 <6567mm@gmail.com>
2018-02-27 19:45:25 -0800

    target code on jevois working

--- linefollowpy/src/Modules/LineFollowPy/LineFollowPy.py
+++ linefollowpy/src/Modules/LineFollowPy/LineFollowPy.py
@@ -57,38 +57,34 @@ class LineFollowPy:
 
 # ######## user code here
 
-imhsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-lowerBound=np.array([0,0,0])
-# note that h and s aren't the 0,0 I though by using the online hsv calc
-upperBound=np.array([180,255,40])
-immask=cv2.inRange(imhsv,lowerBound,upperBound)
+        imhsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        lowerBound=np.array([0,0,0])
+        # note that h and s aren't the 0,0 I though by using the online hsv calc
+        upperBound=np.array([180,255,40])
+        immask=cv2.inRange(imhsv,lowerBound,upperBound)
 
-kernelOpen=np.ones((5,5))
-kernelClose=np.ones((20,20))
+        kernelOpen=np.ones((5,5))
+        kernelClose=np.ones((20,20))
 
-immaskOpen=cv2.morphologyEx(immask,cv2.MORPH_OPEN,kernelOpen)
-immaskClose=cv2.morphologyEx(immaskOpen,cv2.MORPH_CLOSE,kernelClose)
-_, conts, _ = cv2.findContours(immaskClose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        immaskOpen=cv2.morphologyEx(immask,cv2.MORPH_OPEN,kernelOpen)
+        immaskClose=cv2.morphologyEx(immaskOpen,cv2.MORPH_CLOSE,kernelClose)
+        _, conts, _ = cv2.findContours(immaskClose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
-ymax = 0
-xmax = 0
-hmax = 0
-wmax = 0
+        ymax = 0
+        xmax = 0
+        hmax = 0
+        wmax = 0
 
-for i in range(len(conts)):
-    x,y,w,h=cv2.boundingRect(conts[i])
-    if y >= ymax:
-      ymax = y
-      print(y)
-      xmax = x
-      print(x)
-      hmax = h
-      print(h)
-      wmax = w
-      print(w)
-targetx = xmax + int(wmax/2)
-targety = ymax + int(hmax/2)
-cv2.circle(img, (targetx, targety), 10, (0,255,255), -1) 
+        for i in range(len(conts)):
+            x,y,w,h=cv2.boundingRect(conts[i])
+            if y >= ymax:
+                ymax = y
+                xmax = x
+                hmax = h
+                wmax = w
+        targetx = xmax + int(wmax/2)
+        targety = ymax + int(hmax/2)
+        cv2.circle(img, (targetx, targety), 10, (0,255,255), -1) 
 
 
 # ######## end user code
